7cdd6a5fd98fafc8aee2e57087fc2bf0c112f7ae
Felipe Moreira <felipemoreiradecastro@gmail.com>
2021-08-03 18:40:13 -0300

    criação de projeto e migrations

---
+++ README.md
@@ -0,0 +1,19 @@
+## Instalação do projeto
+
+Requisitos:
+<br>PHP 7
+<br>Composer
+<br>npm v6
+<br>node v14
+<br>Mysql
+<br>Git
+
+
+Rode os comandos:
+<br>composer install
+<br>php artisan key:generate
+<br>php artisan migrate --seed
+<br>npm install
+
+Execultar sistema em prompts diferentes
+<br>php artisan serve
---
+++ database/migrations/2021_08_03_213521_create_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_post')->nullable();
+            $table->foreign('id_post')->references('id')->on('posts');
+            $table->text("title");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
